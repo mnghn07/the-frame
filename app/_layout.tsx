1410e71e0f75c0b666e856f8e0360bbd11b36019
Minh Nguyen <25219605+minhnguyen0712@users.noreply.github.com>
2024-01-30 01:47:58 +0700

    feat(:construction:): Adding screens layout with expo-router

---
+++ app/_layout.tsx
@@ -0,0 +1,48 @@
+import { ThemeProvider } from "@shopify/restyle";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import React, { useCallback, useEffect } from "react";
+import { useColorScheme } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import themes from "themes";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded, error] = useFonts({
+    Montserrat: require("../assets/fonts/Montserrat-Regular.ttf")
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (loaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider theme={colorScheme === "dark" ? themes.dark : themes.light}>
+      <SafeAreaProvider onLayout={onLayoutRootView}>
+        <StatusBar style="auto" />
+        <Stack>
+          <Stack.Screen name="home" options={{ headerShown: false }} />
+          {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+          {/* <Stack.Screen name="(auth)" options={{ headerShown: false }} /> */}
+
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        </Stack>
+      </SafeAreaProvider>
+    </ThemeProvider>
+  );
+}
---
+++ app/home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
